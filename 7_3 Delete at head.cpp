ec7fde816ba1cc81ea3984d12ccc516c7e75b6c5
tanvir-alc <tafimahmed222@gmail.com>
2025-07-30 01:29:18 +0600

    delete head

---
+++ 7_3 Delete at head.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val)
+    {
+        this->val =val;
+        this->next=NULL;
+    }
+};
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+ 
+    Node* new_node =new Node(val);
+    if (head==NULL)
+    {
+       head=new_node;
+       tail = new_node;
+       return;
+    } 
+    tail->next=new_node;
+    tail=tail->next;
+}
+
+void print(Node* head)
+{
+    Node * tmp =head;
+    while (tmp!= NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    
+}
+void delete_head_node(Node* &head)
+{
+    Node *delete_node = head;
+    head=head->next;
+    delete delete_node;
+
+}
+int main() {
+
+//    Node* head = new Node(10);
+//    Node* a = new Node(20);
+//    Node* tail= new Node(30);
+//    head->next=a;
+//    a->next=tail;
+Node* head = NULL;
+Node* tail = NULL;
+int val;
+while(1)
+{
+    cin>>val;
+    if(val==-1)
+    {
+        break;
+    }
+    insert_at_tail(head,tail,val);
+}
+delete_head_node(head);
+    print(head);
+    
+    
+    return 0;
+}
+
+
+
