5cbaaeac818fefe428f564016f5c90fbc07b9117
tanvir-alc <tafimahmed222@gmail.com>
2025-07-30 22:31:46 +0600

    any position

---
+++ 7_5 Delete at any position.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int val;
+    Node* next;
+
+    Node(int val)
+    {
+        this->val =val;
+        this->next=NULL;
+    }
+};
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+ 
+    Node* new_node =new Node(val);
+    if (head==NULL)
+    {
+       head=new_node;
+       tail = new_node;
+       return;
+    } 
+    tail->next=new_node;
+    tail=tail->next;
+}
+
+void print(Node* head)
+{
+    Node * tmp =head;
+    while (tmp!= NULL)
+    {
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    
+}
+void delete_any_position(Node* &head,int idx)
+{
+    Node *tmp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        tmp=tmp->next;
+    }
+    Node* delete_node =tmp->next;
+    tmp->next =tmp->next->next;
+    delete delete_node;
+
+}
+int main() {
+
+Node* head = NULL;
+Node* tail = NULL;
+int val;
+while(1)
+{
+    cin>>val;
+    if(val==-1)
+    {
+        break;
+    }
+    insert_at_tail(head,tail,val);
+}
+delete_any_position(head,2);
+    print(head);
+    
+    
+    return 0;
+}
+
+
+
